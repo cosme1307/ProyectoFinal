abca7db8b0508c9128ec540416795c7a373e61bd
alexDz2019 <jonathanmoreno.jm49@hotmail.com>
2019-11-14 19:55:20 -0600

    Comentando el código

--- app/src/main/java/com/example/proyectofinal/RegisterRequestCoro.java
+++ app/src/main/java/com/example/proyectofinal/RegisterRequestCoro.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 public class RegisterRequestCoro extends StringRequest {
 
+    //Aquí se pone la dirección de donde está el archivo php para guardar los datos en la base de datos
+    //Junto con la ip local
     private static final String REGISTER_REQUEST_URL ="http://192.168.43.68/sis22/Coro.php" ;
     private Map<String, String> params;
     public RegisterRequestCoro(String titulo, String autor, String letra, Response.Listener<String> listener){
@@ -15,8 +17,7 @@ public class RegisterRequestCoro extends StringRequest {
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params=new HashMap<>();
 
-
-
+        //Estos son los parametros que recibe la tabla
         params.put("titulo", titulo);
         params.put("autor", autor);
         params.put("letra", letra);
