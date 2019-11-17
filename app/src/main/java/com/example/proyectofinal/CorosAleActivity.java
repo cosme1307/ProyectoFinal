311a8da88bbe19ee994277fe917fb284869fe17c
nova1sweet <bocanitail@gmail.com>
2019-11-17 16:31:08 -0600

    Creando método para guardar coros

--- app/src/main/java/com/example/proyectofinal/CorosAleActivity.java
+++ app/src/main/java/com/example/proyectofinal/CorosAleActivity.java
@@ -3,8 +3,18 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class CorosAleActivity extends AppCompatActivity {
+    private EditText ettitulocal, etautorcal, etletracal;
+    private Button btnRegistrarcal;
+    private ListView lvdatoscal;
+    private AsyncHttpClient clientecal = new AsyncHttpClient();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
