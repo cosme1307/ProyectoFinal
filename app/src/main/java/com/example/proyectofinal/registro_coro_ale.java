23108a94117ef96b5c45308cee237418bd03a76f
Cosme1307 <Cosme1307@hotmail.com>
2019-11-17 17:00:04 -0600

    Creando método para listar los coros

--- app/src/main/java/com/example/proyectofinal/registro_coro_ale.java
+++ app/src/main/java/com/example/proyectofinal/registro_coro_ale.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class registro_coro_ale extends AppCompatActivity {
     private ListView lvdatoscal;
@@ -15,5 +18,26 @@ public class registro_coro_ale extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_coro_ale);
+
+        lvdatoscal = findViewById(R.id.lvDatosale);
+        clientecal = new AsyncHttpClient();
+        obtenerCoros();
+    }
+
+    private void obtenerCoros(){
+        String url = "https://appmovilgamez.000webhostapp.com/obtenerCoroAle.php";
+        clientecal.post(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200){
+                    listarCoros(new String(responseBody));
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
     }
 }
