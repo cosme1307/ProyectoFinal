6b2b8405fd5e4aa57539d91f141c322baa4b7ab7
Cosme1307 <Cosme1307@hotmail.com>
2019-11-17 17:01:44 -0600

    Creando método para listar los coros

--- app/src/main/java/com/example/proyectofinal/registro_coro_ale.java
+++ app/src/main/java/com/example/proyectofinal/registro_coro_ale.java
@@ -1,12 +1,21 @@
 package com.example.proyectofinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -40,4 +49,47 @@ public class registro_coro_ale extends AppCompatActivity {
             }
         });
     }
+
+    //*****************
+
+
+    private  void listarCoros(String respuesta){
+        final ArrayList<CorosAle> listar = new ArrayList<CorosAle>();
+        try{
+            JSONArray jsonArreglo = new JSONArray(respuesta);
+            for (int i=0; i<jsonArreglo.length(); i++){
+                CorosAle a = new CorosAle();
+                a.setId(jsonArreglo.getJSONObject(i).getInt("id_cale"));
+                a.setTitulo(jsonArreglo.getJSONObject(i).getString("titulo"));
+                a.setAutor(jsonArreglo.getJSONObject(i).getString("autor"));
+                a.setLetra(jsonArreglo.getJSONObject(i).getString("letra"));
+
+                listar.add(a);
+
+            }
+
+            ArrayAdapter<CorosAle> a = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, listar);
+            lvdatoscal.setAdapter(a);
+
+            lvdatoscal.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    CorosAle a = listar.get(position);
+                    String url = "https://appmovilgamez.000webhostapp.com/eliminarale.php?id_cale="+a.getId();
+
+
+                    return true;
+                }
+            });
+
+
+        }catch(Exception el){
+            el.printStackTrace();
+        }
+
+
+    }
+
+    //***************
 }
