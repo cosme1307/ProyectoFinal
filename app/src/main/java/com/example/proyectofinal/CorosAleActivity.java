34458001f8cd4df61658b61e50633e45f427e571
nova1sweet <bocanitail@gmail.com>
2019-11-17 16:32:23 -0600

    Creando vista para guardar coros

--- app/src/main/java/com/example/proyectofinal/CorosAleActivity.java
+++ app/src/main/java/com/example/proyectofinal/CorosAleActivity.java
@@ -20,5 +20,7 @@ public class CorosAleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coros_ale);
+
+
     }
 }
